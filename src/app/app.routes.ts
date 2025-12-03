d7eb6688fd8a8c6f674cb79814a83eb4dbc00e46
michael200505 <emuya@est.ups.edu.ec>
2025-12-03 13:41:33 -0500

    Firebase console

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,64 +2,78 @@ import { Routes } from '@angular/router';
 import { roleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
+
+  // -----------------------------
+  // PUBLIC ROUTES
+  // -----------------------------
   {
     path: '',
-    loadComponent: () => import('./pages/home/home').then(m => m.PublicHomeComponent)
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.PublicHomeComponent)
   },
+
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
+    loadComponent: () =>
+      import('./pages/login/login').then(m => m.LoginComponent)
   },
+
   {
-  path: 'admin',
-  loadComponent: () => import('./pages/admin/admin').then(m => m.AdminPanelComponent),
-  canActivate: [roleGuard],
-  data: { role: 'admin' }
-},
+    path: 'portfolio/:id',
+    loadComponent: () =>
+      import('./pages/portfolio/portfolio').then(m => m.PortfolioComponent)
+  },
+
+  {
+    path: 'agendar/:id',
+    loadComponent: () =>
+      import('./pages/agendar/agendar').then(m => m.AgendarAsesoriaComponent)
+  },
+
+  {
+    path: 'mis-asesorias',
+    loadComponent: () =>
+      import('./pages/mis-asesorias/mis-asesorias').then(m => m.MisAsesoriasComponent)
+  },
+
+  {
+    path: 'denied',
+    loadComponent: () =>
+      import('./pages/denied/denied').then(m => m.DeniedComponent)
+  },
+
+  // -----------------------------
+  // PROTECTED ROUTES
+  // -----------------------------
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./pages/admin/admin').then(m => m.AdminPanelComponent),
+    canActivate: [roleGuard],
+    data: { role: 'admin' }
+  },
 
   {
     path: 'programmer',
-    loadComponent: () => import('./pages/programmer/programmer').then(m => m.ProgrammerPanelComponent),
+    loadComponent: () =>
+      import('./pages/programmer/programmer').then(m => m.ProgrammerPanelComponent),
     canActivate: [roleGuard],
     data: { role: 'programmer' }
   },
+
   {
-    path: 'denied',
-    loadComponent: () => import('./pages/denied/denied').then(m => m.DeniedComponent)
+    path: 'programmer/new-project',
+    loadComponent: () =>
+      import('./pages/programmer/project-form').then(m => m.ProjectFormComponent),
+    canActivate: [roleGuard],
+    data: { role: 'programmer' }
   },
+
+  // -----------------------------
+  // WILDCARD ROUTE (AL FINAL)
+  // -----------------------------
   {
     path: '**',
     redirectTo: ''
-  },
-  {
-  path: 'programmer',
-  loadComponent: () => import('./pages/programmer/programmer').then(m => m.ProgrammerPanelComponent),
-  canActivate: [roleGuard],
-  data: { role: 'programmer' }
-},
-{
-  path: 'programmer/new-project',
-  loadComponent: () => import('./pages/programmer/project-form').then(m => m.ProjectFormComponent),
-  canActivate: [roleGuard],
-  data: { role: 'programmer' }
-},
-
-{
-  path: '',
-  loadComponent: () => import('./pages/home/home').then(m => m.PublicHomeComponent)
-},
-{
-  path: 'portfolio/:id',
-  loadComponent: () => import('./pages/portfolio/portfolio').then(m => m.PortfolioComponent)
-},
-{
-  path: 'agendar/:id',
-  loadComponent: () => import('./pages/agendar/agendar').then(m => m.AgendarAsesoriaComponent)
-},
-{
-  path: 'mis-asesorias',
-  loadComponent: () => import('./pages/mis-asesorias/mis-asesorias').then(m => m.MisAsesoriasComponent)
-},
-
-  
+  }
 ];
