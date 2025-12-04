73a097d92d8b9112c1a5b9de6004f3d9ea693cfc
michael200505 <emuya@est.ups.edu.ec>
2025-12-04 16:15:21 -0500

    Interface

--- src/app/pages/admin/admin.ts
+++ src/app/pages/admin/admin.ts
@@ -94,4 +94,44 @@ export class AdminPanelComponent {
     await this.loadUsers();
     await this.loadProgramgers();
   }
+  editProgrammer(p: ProgrammerProfile) {
+  this.selectedUser = {
+    uid: p.uid,
+    email: '',
+    displayName: p.name,
+    photoURL: p.photoURL,
+    role: 'programmer'
+  };
+
+  this.newProgrammer = { ...p };
+}
+async updateProgrammer() {
+  await this.programmerService.updateProgrammer(this.newProgrammer.uid, this.newProgrammer);
+
+  alert("Programador actualizado correctamente ✔");
+
+  this.selectedUser = null;
+  this.newProgrammer = {
+    uid: '',
+    name: '',
+    specialty: '',
+    description: '',
+    photoURL: '',
+    contactLinks: {},
+    socialLinks: {}
+  };
+
+  await this.loadProgramgers();
+}
+async deleteProgrammer(uid: string) {
+  if (!confirm("¿Seguro que deseas eliminar este programador?")) return;
+
+  await this.programmerService.deleteProgrammer(uid);
+  await this.userService.setRole(uid, 'user'); // lo devolvemos a usuario normal
+
+  alert("Programador eliminado ✔");
+
+  await this.loadProgramgers();
+  await this.loadUsers();
+}
 }
